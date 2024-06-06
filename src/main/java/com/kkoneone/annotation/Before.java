975a84015453f6cca2b9ccbbf272bb3ef154a9ff
zhengyi.huang <huangzhengyi.hzy@alibaba-inc.com>
2024-06-06 19:21:11 +0800

    创建一些常用注解

---
+++ src/main/java/com/kkoneone/annotation/Before.java
@@ -0,0 +1,18 @@
+package com.kkoneone.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Author : huangzhengyi.hzy@alibaba-inc.com
+ * @Date : 2024/6/6 19:01
+ * @Description:
+ **/
+//什么类型上可以使用这个注解
+@Target(ElementType.METHOD)
+//什么时候可以获取这个反射
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Before {
+}
